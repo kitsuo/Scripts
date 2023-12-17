57540f9ee930f4e3ab9a9a1f1ba10b119a2d3e2b
kitsuo <arbi.sp23@gmail.com>
2023-12-17 19:56:53 +0100

    add  epel-release

--- script.sh
+++ script.sh
@@ -14,17 +14,18 @@ echo "User $username created and added to the sudo group."
 # Check the package manager
 if command -v apt-get >/dev/null 2>&1; then
     package_manager="apt-get"
+    PACKAGES="htop vim nmap fail2ban"
 elif command -v yum >/dev/null 2>&1; then
     package_manager="yum"
+    PACKAGES="htop vim-enhanced nmap fail2ban"
 elif command -v dnf >/dev/null 2>&1; then
     package_manager="dnf"
+    dnf update -y && dnf install -y epel-release
+    PACKAGES="htop vim-enhanced nmap fail2ban"
 else
     echo "Unsupported distribution. Unable to determine the package manager."
     exit 1
 fi
-
-# Package variables
-PACKAGES=" htop vim nmap fail2ban"
 
 # Install packages
 echo "Installing packages using $package_manager..."
